da7e3843383d3c2828622eca754cd11cd4e47a74
Lucia Mandová <lucka.mandova@gmail.com>
2024-05-31 20:09:55 +0200

    feat: Add transfer card & update trading card

--- patty-fe/pages/funding/[id].vue
+++ patty-fe/pages/funding/[id].vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <trading-card name="Doggies" token="BTC" />
+  <div class="flex flex-col gap-10">
+    <trading-card
+      :tokenName="funding?.tokenName"
+      :tokenSymbol="funding?.tokenSymbol"
+    />
+    <transfers-card :tokenName="funding?.tokenName" />
   </div>
 </template>
+<script setup lang="ts">
+import { useFundingService } from '~/server/services';
+
+const service = useFundingService();
+const { params } = useRoute();
+const funding = service.getFundingById(params.id as string);
+</script>
--- patty-fe/server/services/funding-service.ts
+++ patty-fe/server/services/funding-service.ts
@@ -7,12 +7,39 @@ export function useFundingService() {
       title: 'Funding 1',
       description: 'Description 1',
       imageSrc: 'img/prop_cat.png',
+      tokenName: 'Cattie',
+      tokenSymbol: 'CAT',
     },
     {
       id: '2',
       title: 'Funding 2',
       description: 'Description 2',
       imageSrc: 'img/prop_dino.png',
+      tokenName: 'Dino',
+      tokenSymbol: 'DINO',
+    },
+  ];
+
+  const transfers = [
+    {
+      id: '1',
+      fundingId: '1',
+      account: '0xf0A49418375CfD4A33CA316C2522e65F46e3f909',
+      type: 'buy',
+      eth: 0.00002,
+      token: 0.1,
+      date: '5 seconds ago',
+      transaction: '0xf0A49418375CfD4A33CA316C2522e65F46e3f909',
+    },
+    {
+      id: '2',
+      fundingId: '1',
+      account: '0xf0A49418375CfD4A33CA316C2522e65F46e3f909',
+      type: 'buy',
+      eth: 0.00005,
+      token: 1.345,
+      date: '15 seconds ago',
+      transaction: '0xf0A49418375CfD4A33CA316C2522e65F46e3f909',
     },
   ];
 
@@ -20,5 +47,13 @@ export function useFundingService() {
     return fundings;
   };
 
-  return { getFundings };
+  const getFundingById = (id: string) => {
+    return fundings.find((funding) => funding.id === id);
+  };
+
+  const getTransfersById = (id: string) => {
+    return transfers.filter((transfer) => transfer.fundingId === id);
+  };
+
+  return { getFundings, getFundingById, getTransfersById };
 }
