20028b8dee7ed3c9a8d47b20f16f67cb2a313a9f
Viki Val <viktorko99@gmail.com>
2024-05-31 18:57:19 +0200

    :bug: remappings for v4

--- contracts/src/PatPool.sol
+++ contracts/src/PatPool.sol
@@ -1,7 +1,38 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-contract PatPool {
+import {IPoolManager} from "@v4-core/interfaces/IPoolManager.sol";
+import {IHooks} from "@v4-core/interfaces/IHooks.sol";
+import {PoolKey} from "@v4-core/types/PoolKey.sol";
+import {CurrencyLibrary, Currency} from "@v4-core/types/Currency.sol";
 
+contract PoolInitialize {
+    using CurrencyLibrary for Currency;
+
+    // set the initialize router
+    IPoolManager manager = IPoolManager(address(0x01));
+
+    function init(
+        address token0,
+        address token1,
+        uint24 swapFee,
+        int24 tickSpacing,
+        address hook,
+        uint160 sqrtPriceX96,
+        bytes calldata hookData
+    ) external {
+        // sort your tokens! v4 requires token0 < token1
+        if (token0 > token1) {
+            (token0, token1) = (token1, token0);
+        }
+
+        PoolKey memory pool = PoolKey({
+            currency0: Currency.wrap(token0),
+            currency1: Currency.wrap(token1),
+            fee: swapFee,
+            tickSpacing: tickSpacing,
+            hooks: IHooks(hook)
+        });
+        manager.initialize(pool, sqrtPriceX96, hookData);
+    }
 }
