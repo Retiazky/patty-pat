602d588dae12c1d1030719d7468644742b23d764
Lucia Mandová <lucka.mandova@gmail.com>
2024-05-31 17:23:43 +0200

    feat: Add voting card component

---
+++ patty-fe/components/VotingCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <s-card class="w-1/2 border-2 rounded-lg">
+    <s-card-header>
+      <s-card-title>{{ proposal.title }}</s-card-title>
+      <s-card-description>
+        Voting until: {{ proposal.endingDateTime }}
+      </s-card-description>
+    </s-card-header>
+    <s-card-content>
+      <p>{{ proposal.description }}</p>
+      <p>Minimal amount: {{ proposal.minAmount }} $</p>
+      <div class="flex justify-center items-center">
+        <img class="w-1/2 h-1/2 rounded-lg" :src="proposal.imageSrc" />
+      </div>
+    </s-card-content>
+    <s-card-footer class="flex gap-2">
+      <s-button class="flex-1 border-2" variant="success">For</s-button>
+      <s-button class="flex-1 border-2" variant="destructive">Against</s-button>
+      <s-button class="flex-1 border-2" variant="ghost">Abstain</s-button>
+    </s-card-footer>
+  </s-card>
+</template>
+<script setup lang="ts">
+import type { Proposal } from '~/types';
+defineProps<{
+  proposal: Proposal;
+}>();
+</script>
--- patty-fe/pages/proposals/index.vue
+++ patty-fe/pages/proposals/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <div class="flex flex-col gap-10 items-center">
+      <voting-card v-for="proposal in proposals" :proposal="proposal" />
+    </div>
     <span
       class="rounded-full absolute bottom-8 right-4 border cursor-pointer p-2 bg-white shadow-md hover:bg-accent/80"
       @click="$router.push('/proposals/create')"
@@ -8,3 +11,25 @@
     </span>
   </div>
 </template>
+<script setup lang="ts">
+import type { Proposal } from '~/types';
+
+const proposals: Proposal[] = [
+  {
+    title: 'Proposal 1',
+    description: 'Description 1',
+    minAmount: 100,
+    imageSrc: 'img/prop_cat.png',
+    votes: 0,
+    endingDateTime: '2024-08-31 12:00',
+  },
+  {
+    title: 'Proposal 2',
+    description: 'Description 2',
+    minAmount: 200,
+    imageSrc: 'img/prop_dino.png',
+    votes: 0,
+    endingDateTime: '2024-08-31 13:00',
+  },
+];
+</script>
