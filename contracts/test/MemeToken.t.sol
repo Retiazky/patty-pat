844971c3d5ab11b50aefaf7a4ac52a13c73e4207
Viki Val <viktorko99@gmail.com>
2024-06-01 11:38:42 +0200

    :test_tube: Meme Token

---
+++ contracts/test/MemeToken.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MemeToken} from "src/MemeToken.sol";
+
+
+contract MemeTokenTest is Test {
+    MemeToken public token;
+
+    function setUp() public {
+        address initialOwner = vm.addr(1);
+        console.log("Initial owner: %s", initialOwner);
+        token = new MemeToken(initialOwner, "CatWithCoco", "CWC");
+    }
+
+    function testSupply() public {
+        assertEq(token.totalSupply(), 10000000000000000 * 10 ** token.decimals());
+    }
+}
+
+// DAO
