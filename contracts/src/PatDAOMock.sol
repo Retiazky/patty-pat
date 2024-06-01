0406d6715f6d6f114133152fdb26849737bf646b
TetianaKukhelna <grey.shadow@ukr.net>
2024-06-01 20:40:19 +0200

    added: create memeToken, initialization pool, added liquidity

--- contracts/src/PatDAOMock.sol
+++ contracts/src/PatDAOMock.sol
@@ -2,8 +2,30 @@
 pragma solidity ^0.8.20;
 
 import {IPatDAO, Campaign, CampaingCreated, BuybackCreated} from "./interfaces/IPatDAO.sol";
+import {PoolKey} from "@v4-core/types/PoolKey.sol";
+import {Currency,CurrencyLibrary} from "@v4-core/types/Currency.sol";
+import {IPoolManager} from "@v4-core/interfaces/IPoolManager.sol";
+import {IHooks} from "@v4-core/interfaces/IHooks.sol";
+import {PoolModifyLiquidityTest} from "@v4-core/test/PoolModifyLiquidityTest.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {StateLibrary} from "@v4-core/libraries/StateLibrary.sol";
+import {MemeToken} from "src/MemeToken.sol";
+import {PoolManager} from "@v4-core/PoolManager.sol";
+import {PoolId, PoolIdLibrary} from "@v4-core/types/PoolId.sol";
+import {PoolSwapTest} from "@v4-core/test/PoolSwapTest.sol";
+import {TickMath} from "@v4-core/libraries/TickMath.sol";
+import {BalanceDelta} from "@v4-core/types/BalanceDelta.sol";
 
 contract PatDAO is IPatDAO {
+    PoolModifyLiquidityTest public lpRouter;
+    IPoolManager public manager;
+    PoolKey public poolKey;
+    PoolId public poolId;
+
+    using PoolIdLibrary for PoolKey;
+    using StateLibrary for IPoolManager;
+
+
     address public governanceSC;
     mapping(address => Campaign) public campaigns;
 
@@ -14,10 +36,7 @@ contract PatDAO is IPatDAO {
     }
 
     modifier onlyGovernance() {
-        require(
-            msg.sender == governanceSC,
-            "PatDAO: Only Governance can call this function"
-        );
+        require(msg.sender == governanceSC, "PatDAO: Only Governance can call this function");
         _;
     }
 
@@ -34,8 +53,50 @@ contract PatDAO is IPatDAO {
         uint256 supply
     ) public onlyGovernance {
         // TODO: Implement
-        address token = address(0);
+
+        manager = IPoolManager(address(0x4abee8d464c1d7367cf31b264344b655cf577f0f));
+
+        lpRouter = new PoolModifyLiquidityTest(manager);
+
+        MemeToken token = new MemeToken(governanceSC, name, symbol);
+
+        address token0 = address(0);
+        address token1 = address(token);
+        uint24 swapFee = 0; // 0.05% fee tier
+        int24 tickSpacing = 1;
+        uint160 startingPrice = 7922816251426433759354395033600;
+
         campaigns[token] = Campaign(name, symbol, uri, supply, feeRecipient);
+
+        bytes memory hookData = new bytes(0);
+        poolKey = PoolKey({
+            currency0: CurrencyLibrary.NATIVE,
+            currency1: Currency.wrap(token1),
+            fee: swapFee,
+            tickSpacing: 1,
+            hooks: IHooks(address(0x0)) // !!! Hookless pool is address(0x0)
+        });
+
+        manager.initialize(poolKey, startingPrice, hookData);
+        poolId = poolKey.toId();
+        IERC20(token1).approve(address(lpRouter), type(uint256).max);
+
+        int24 tickLower = -400;
+        int24 tickUpper = 0;
+        int256 liquidityDelta = 10e18;
+
+
+        BalanceDelta result = lpRouter.modifyLiquidity{value: 10 ether}(
+            poolKey,
+            IPoolManager.ModifyLiquidityParams({
+                tickLower: tickLower,
+                tickUpper: tickUpper,
+                liquidityDelta: liquidityDelta,
+                salt: 0
+            }),
+            new bytes(0)
+        );
+
         emit CampaingCreated(name, symbol, uri, supply, token, feeRecipient);
     }
 
@@ -44,11 +105,7 @@ contract PatDAO is IPatDAO {
         delete campaigns[token];
     }
 
-    function createBuyback(
-        address token,
-        uint256 amountIn,
-        uint256 amountOut
-    ) public onlyGovernance {
+    function createBuyback(address token, uint256 amountIn, uint256 amountOut) public onlyGovernance {
         // TODO: Implement
         emit BuybackCreated(token, amountIn, amountOut);
     }
