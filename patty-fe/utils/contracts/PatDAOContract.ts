3673190d17ea5b533f42d3290fa789b3762d53d7
Branislav Hozza <brankohozza@gmail.com>
2024-06-01 12:27:21 +0200

    feat: contracts

---
+++ patty-fe/utils/contracts/PatDAOContract.ts
@@ -0,0 +1,6 @@
+import { abi } from "@/utils/abi/PatDAO";
+
+export const patDAOContract = {
+  address: "0x4C2a1Bf6d7a0cFd082587d0C376530d1c28da0e7",
+  abi,
+} as const;
---
+++ patty-fe/utils/contracts/PatGovernorContract.ts
@@ -0,0 +1,6 @@
+import { abi } from "@/utils/abi/PatGovernor";
+
+export const patGovernorContract = {
+  address: "0xDFAc8F04f85a85F513E813A33CB16B155e6BfcCb",
+  abi,
+} as const;
